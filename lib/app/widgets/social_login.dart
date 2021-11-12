7482466283c38e5b4d7ac6e2f2a03df944ab1801
Xercis Silao <xercis.silao@gmail.com>
2021-11-12 10:59:37 +0800

    update social login design

--- lib/app/widgets/social_login.dart
+++ lib/app/widgets/social_login.dart
@@ -1,4 +1,3 @@
-import 'package:dwellu/app/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
 import 'dart:io' show Platform;
 
@@ -20,55 +19,30 @@ class SocialLogin extends StatelessWidget {
     Color backgroundColor = Colors.white,
     Border border,
     @required AssetImage image,
-    double imageHeight = 15.0,
-    double imageWidth = 15.0,
+    double imageHeight = 25.0,
+    double imageWidth = 25.0,
     @required Function onPressed
   }){
-    double borderRadius = 24.0;
-    
-    return Material(
-      elevation: 5.0,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderRadius)),
-      child: Container(
-        constraints: BoxConstraints(maxWidth: 200),
-        height: 48.0,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
-          color: backgroundColor,
-          border: border
-        ),
-        child: FlatButton(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(borderRadius))
+
+    return Tooltip(
+      message: text ?? '',
+      child: GestureDetector(
+        onTap: onPressed,
+        child: Container(
+          width: 50,
+          height: 50,
+          decoration: BoxDecoration(
+            color: backgroundColor ?? Colors.transparent,
+            shape: BoxShape.circle,
+            border: border
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                padding: EdgeInsets.only(right: 6.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(borderRadius),
-                ),
-                child: Center(
-                  child: Image(
-                    image: image,
-                    height: imageHeight,
-                    width: imageWidth,
-                  ),
-                ),
-              ),
-              Flexible(
-                child: CustomText(
-                  text: text,
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w500,
-                  color: textColor,
-                  textAlign: TextAlign.center
-                ),
-              ),
-            ]
+          child: Center(
+            child: Image(
+              image: image,
+              height: imageHeight,
+              width: imageWidth,
+            )
           ),
-          onPressed: onPressed,
         ),
       ),
     );
@@ -76,56 +50,46 @@ class SocialLogin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            /** ===============================    LOGIN WITH FACEBOOK           ============================================ */
-            Flexible(
-              child: brookySocialButton(
-                text: 'Facebook',
-                backgroundColor: Color(0xff3b5999),
-                textColor: Colors.white,
-                image: AssetImage(
-                  "graphics/flogo-HexRBG-Wht-100.png",
-                  package: "flutter_auth_buttons",
-                ),
-                onPressed: facebookHandleSignIn,
-              )
-            ),
-            SizedBox(width: 10.0),
-            /** ===============================    LOGIN WITH GOOGLE           ============================================ */
-            Flexible(
-              child: brookySocialButton(
-                text: 'Google',
-                image: AssetImage(
-                  "graphics/google-logo.png",
-                  package: "flutter_auth_buttons",
-                ),
-                border: Border.all(
-                  color: Colors.grey[300]
-                ),
-                onPressed: googleHandleSignIn,
-              ),
-            )
-          ],
-        ),
-        SizedBox(height: 10.0),
         /** ===============================    LOGIN WITH APPLE           ============================================ */
-        Platform.isIOS ?
+        !Platform.isIOS ?
         brookySocialButton(
           text: 'Sign in with Apple',
           textColor: Colors.white,
           backgroundColor: Colors.black,
-          imageHeight: 17.0,
-          imageWidth: 17.0,
           image: AssetImage(
             "graphics/apple_logo_white.png",
             package: "flutter_auth_buttons",
           ),
           onPressed: appleHandleSignIn,
         ) : Container(),
+        SizedBox(width: 20.0),
+        /** ===============================    LOGIN WITH FACEBOOK           ============================================ */
+        brookySocialButton(
+          text: 'Facebook',
+          backgroundColor: Color(0xff3b5999),
+          textColor: Colors.white,
+          image: AssetImage(
+            "graphics/flogo-HexRBG-Wht-100.png",
+            package: "flutter_auth_buttons",
+          ),
+          onPressed: facebookHandleSignIn,
+        ),
+        SizedBox(width: 20.0),
+        /** ===============================    LOGIN WITH GOOGLE           ============================================ */
+        brookySocialButton(
+          text: 'Google',
+          image: AssetImage(
+            "graphics/google-logo.png",
+            package: "flutter_auth_buttons",
+          ),
+          border: Border.all(
+            color: Colors.grey[300]
+          ),
+          onPressed: googleHandleSignIn,
+        ),
       ],
     );
   }
