832d5990ad1867785b0610e8233e197cac6cfa86
Xercis Silao <xercis.silao@gmail.com>
2021-11-15 15:21:48 +0800

    flat button

---
+++ lib/app/widgets/form_fields/custom_flat_button.dart
@@ -0,0 +1,41 @@
+import 'package:dwellu/app/widgets/custom_text.dart';
+import 'package:flutter/material.dart';
+
+class CustomFlatButton extends StatelessWidget {
+  final String text;
+  final double fontSize;
+  final Color color;
+  final Function onPressed;
+  final FontWeight fontWeight;
+  final EdgeInsets padding;
+  
+
+  CustomFlatButton({
+    @required this.text,
+    @required this.onPressed,
+    this.fontSize = 14.0,
+    this.color = Colors.blue,
+    this.fontWeight = FontWeight.normal,
+    this.padding = const EdgeInsets.all(8.0),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      onPressed: onPressed,
+      minWidth: 0,
+      height: 0,
+      padding: EdgeInsets.zero,
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      child: Container(
+        padding: padding,
+        child: CustomText(
+          text: text,
+          fontSize: fontSize,
+          color: color,
+          fontWeight: fontWeight,
+        ),
+      )
+    );
+  }
+}
