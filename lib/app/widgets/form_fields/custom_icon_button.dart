815a55c884773c5cb11af8ff535dcc4d720b5153
Xercis Silao <xercis.silao@gmail.com>
2021-11-17 16:26:01 +0800

    update custom icon button

--- lib/app/widgets/form_fields/custom_icon_button.dart
+++ lib/app/widgets/form_fields/custom_icon_button.dart
@@ -2,11 +2,18 @@ import 'package:dazle/app/utils/app.dart';
 import 'package:flutter/material.dart';
 
 class CustomIconButton extends StatelessWidget {
+  final bool main;
+  final Color backgroudColor;
+  final double borderRadius;
+
   final IconData iconData;
   final Function onPressed;
   final Alignment alignment;
 
   CustomIconButton({
+    this.main = true,
+    this.backgroudColor = App.appMainColor,
+    this.borderRadius = 5.0,
     this.iconData = Icons.arrow_right_alt,
     @required this.onPressed,
     this.alignment = Alignment.centerRight
@@ -16,17 +23,20 @@ class CustomIconButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       alignment: alignment,
-      constraints: BoxConstraints(maxWidth: 300, minWidth: 250),
+      constraints: BoxConstraints(maxWidth: 300, minWidth: 100),
       child: FlatButton(
         materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
         padding: EdgeInsets.symmetric(vertical: 6.0),
-        color: App.appMainColor,
+        color: main ? backgroudColor : Colors.white,
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(5.0))
+          side: BorderSide(
+            color: backgroudColor,
+            width: 1.5
+          )
         ),
         child: Icon(
           iconData, //Icons.arrow_right_alt,
-          color: Colors.white,
+          color: main ? Colors.white : backgroudColor,
           size: 35.0,
         ),
         onPressed: onPressed
