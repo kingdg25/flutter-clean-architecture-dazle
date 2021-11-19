e21282ac1e9e85164756f6a72ab0d604cab798f6
Xercis Silao <xercis.silao@gmail.com>
2021-11-19 15:05:25 +0800

    update  app name, color, etc

--- lib/app/utils/app.dart
+++ lib/app/utils/app.dart
@@ -1,6 +1,21 @@
+import 'package:dazle/domain/entities/todo_user.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert' as convert;
 
 class App{
-  static const appMainColor = Color.fromRGBO(51, 212, 157, 1.0);
-  static const appTextColor = Color.fromRGBO(75, 70, 70, 1.0);
+  static const name = "Dazle";
+
+  static const mainColor = Color.fromRGBO(51, 212, 157, 1.0);
+  static const fieldTextColor = Color.fromRGBO(75, 70, 70, 1.0);
+  static const textColor = Color.fromRGBO(46, 53, 61, 1.0);
+
+  static getUser() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    var user = prefs.getString('user');
+    var todoUser = convert.jsonDecode(user);
+
+    return TodoUser.fromJson(todoUser);
+  }
 }
