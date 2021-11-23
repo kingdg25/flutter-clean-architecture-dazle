bf280ae00060f56d5958898784a9f2e8298261df
Xercis Silao <xercis.silao@gmail.com>
2021-11-23 16:42:41 +0800

    logOutUser

--- lib/app/utils/app.dart
+++ lib/app/utils/app.dart
@@ -1,5 +1,7 @@
 import 'package:dazle/domain/entities/todo_user.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert' as convert;
 
@@ -22,4 +24,21 @@ class App{
       print('getUser error $e');
     }
   }
+
+  static logOutUser() async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      final GoogleSignIn googleSignIn = GoogleSignIn();
+    
+      await prefs.remove('accessToken');
+      await prefs.remove('user');
+      
+      await FacebookAuth.instance.logOut();
+      await googleSignIn.signOut();
+    
+    } catch (e) {
+      print('getUser error $e');
+    }
+  }
+
 }
