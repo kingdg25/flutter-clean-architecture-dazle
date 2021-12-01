cb92973ec9e4716411f91f3078de073457ed79e0
Xercis Silao <xercis.silao@gmail.com>
2021-12-01 15:36:26 +0800

    InviteTile entity

---
+++ lib/domain/entities/invite_tile.dart
@@ -0,0 +1,34 @@
+class InviteTile {
+  final String id;
+  final String photoURL;
+  final String firstName;
+  final String lastName;
+  final String totalConnection;
+
+  String get displayName{
+    return "$firstName $lastName";
+  }
+
+  InviteTile(
+    this.id,
+    this.photoURL,
+    this.firstName, 
+    this.lastName, 
+    this.totalConnection
+  );
+
+  InviteTile.fromJson(Map<String, dynamic> json)
+    : id = json['_id'],
+      photoURL = json['photo_url'],
+      firstName = json['firstname'],
+      lastName = json['lastname'],
+      totalConnection = json['total_connection'];
+
+  Map<String, dynamic> toJson() => {
+    '_id': id,
+    'photo_url': photoURL,
+    'firstname': firstName,
+    'lastname': lastName,
+    'total_connection': totalConnection
+  };
+}
