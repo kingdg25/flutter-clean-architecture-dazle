812f8b796be0a800e626d84c0be449201c098eef
Xercis Silao <xercis.silao@gmail.com>
2021-12-09 17:26:46 +0800

    header pic with search field and text.PropertyListTile.PropertyDetailsListTile

---
+++ lib/app/pages/home/components/property_list_tile.dart
@@ -0,0 +1,86 @@
+import 'package:dazle/app/utils/app.dart';
+import 'package:dazle/app/utils/app_constant.dart';
+import 'package:dazle/app/widgets/custom_text.dart';
+import 'package:dazle/app/widgets/form_fields/custom_flat_button.dart';
+import 'package:flutter/material.dart';
+
+class PropertyListTile extends StatelessWidget {
+  final String title;
+  final String subTitle;
+
+  PropertyListTile({
+    @required this.title,
+    @required this.subTitle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 20, bottom: 20, left: 20),
+      decoration: AppConstant.bottomBorder,
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CustomText(
+                  text: title,
+                  fontSize: 19,
+                  fontWeight: FontWeight.w600,
+                ),
+                Container(
+                  width: 55,
+                  height: 17,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color.fromRGBO(240, 242, 247, 1.0)
+                  ),
+                  child: CustomFlatButton(
+                    text: 'View All',
+                    fontSize: 10,
+                    color: App.hintColor,
+                    fontWeight: FontWeight.w400,
+                    padding: EdgeInsets.zero,
+                    onPressed: () {
+                    }
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerLeft,
+            child: CustomText(
+              text: subTitle,
+              fontSize: 13,
+              fontWeight: FontWeight.w400,
+              color: App.hintColor,
+            ),
+          ),
+          Container(
+            height: 130.0,
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: 8,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (BuildContext context, int index){
+                return Padding(
+                  padding: EdgeInsets.only(top: 8, bottom: 8, right: 10),
+                  child: Container(
+                    width: 250,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.orange
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/app/pages/home/home_view.dart
+++ lib/app/pages/home/home_view.dart
@@ -1,3 +1,7 @@
+import 'package:dazle/app/pages/home/components/property_details_list_tile.dart';
+import 'package:dazle/app/pages/home/components/property_list_tile.dart';
+import 'package:dazle/app/utils/app.dart';
+import 'package:dazle/app/widgets/form_fields/custom_search_field.dart';
 import 'package:dazle/data/repositories/data_home_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
@@ -54,9 +58,130 @@ class _HomePageState extends ViewState<HomePage, HomeController> {
           )
         ],
       ),
-      body: Center(
-        child: Text('home page'),
-      ),
+      body: ControlledWidgetBuilder<HomeController>(
+        builder: (context, controller) {
+          final double screenHeight = MediaQuery.of(context).size.height;
+          final double screenWidth = MediaQuery.of(context).size.width;
+
+          return ListView(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    width: screenWidth,
+                    height: (screenWidth < 375) ? screenHeight/3 : screenHeight/2,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        alignment: Alignment.center,
+                        image: AssetImage('assets/header.jpg'),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: screenWidth,
+                    height: (screenWidth < 375) ? screenHeight/3 : screenHeight/2,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromRGBO(0, 0, 0, 0.25),
+                          blurRadius: 4,
+                          offset: Offset(0, 4), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    top: 0,
+                    left: 1,
+                    right: 1,
+                    child: CustomSearchField(
+                      // controller: controller.searchTextController,
+                      hintText: 'Building, Neighboorhood, City',
+                      iconData: Icons.contacts_outlined,
+                      borderRadius: 10.0,
+                      filled: true,
+                      fillColor: Colors.white,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:  BorderSide(
+                          color: Colors.white
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:  BorderSide(
+                          color: Colors.white
+                        ),
+                      ),
+                      isAssetIcon: true,
+                      asset: 'assets/icons/filter.png',
+                      onChanged: (value) {
+                        // controller.searchUser();
+                      },
+                      suggestionsCallback: (pattern) async {
+                        return null;
+                      },
+                      onSuggestionSelected: (suggestion) {
+                        // controller.searchTextController.text = suggestion;
+                        // controller.getMyConnection(filterByName: suggestion);
+                      },
+                      onSubmitted: (value) {
+                        // controller.getMyConnection(filterByName: value);
+                      },
+                      onPressedButton: () {
+                      },
+                    ),
+                  ),
+                  Positioned(
+                    bottom: screenHeight/14,
+                    left: 20,
+                    right: screenWidth/2.6,
+                    child: Text.rich(
+                      TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "PropMatch\n",
+                            style: TextStyle(
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold
+                            )
+                          ),
+                          TextSpan(
+                            text: "Matching brokers with properties to brokers with clients.",
+                            style: TextStyle(
+                              fontSize: 15.0,
+                              height: 1.5,
+                              fontWeight: FontWeight.w600
+                            )
+                          ),
+                        ]
+                      ),
+                      style: App.textStyle(
+                        color: Colors.white,
+                      )
+                    ),
+                  )
+                ],
+              ),
+              PropertyListTile(
+                title: 'Spotlight',
+                subTitle: 'Most unique and exclusive properties',
+              ),
+              PropertyDetailsListTile(
+                title: 'Matched Properties',
+                subTitle: 'Just listed in the app',
+              ),
+              PropertyListTile(
+                title: 'Why Brooky?',
+                subTitle: 'Take control of the deal, Here’s how',
+              ),
+              PropertyDetailsListTile(
+                title: 'New Homes',
+                subTitle: 'Just listed in the app',
+              ),
+            ],
+          );
+        }
+      )
     );
   }
 }
