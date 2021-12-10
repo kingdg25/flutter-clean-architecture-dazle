54357721314bc943a1c4f9ffa48e5d3ab3716583
Xercis Silao <xercis.silao@gmail.com>
2021-12-10 17:13:32 +0800

    spotLight,matchedProperties,whyBrooky,newHomes repostitories

--- lib/data/repositories/data_home_repository.dart
+++ lib/data/repositories/data_home_repository.dart
@@ -1,4 +1,6 @@
 import 'dart:convert' as convert;
+import 'package:dazle/domain/entities/photo_tile.dart';
+import 'package:dazle/domain/entities/property.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:dazle/domain/repositories/home_repository.dart';
@@ -8,8 +10,18 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 
 class DataHomeRepository extends HomeRepository {
+  List<PhotoTile> spotLight;
+  List<Property> matchedProperties;
+  List<PhotoTile> whyBrooky;
+  List<Property> newHomes;
+
   static DataHomeRepository _instance = DataHomeRepository._internal();
-  DataHomeRepository._internal();
+  DataHomeRepository._internal() {
+    spotLight = <PhotoTile>[];
+    matchedProperties = <Property>[];
+    whyBrooky = <PhotoTile>[];
+    newHomes = <Property>[];
+  }
   factory DataHomeRepository() => _instance;
 
 
@@ -63,6 +75,104 @@ class DataHomeRepository extends HomeRepository {
       };
     }
   
+  }
+
+  @override
+  Future<List<PhotoTile>> getSpotLight() async {
+    return [
+      PhotoTile('', 'Near you'),
+      PhotoTile('http://via.placeholder.com/200x150', 'Lorem Ipsum'),
+      PhotoTile('http://via.placeholder.com/350x150', 'text'),
+      PhotoTile('https://images.unsplash.com/photo-1532264523420-881a47db012d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9', 'text2'),
+      PhotoTile('https://picsum.photos/250?image=9', 'text3')
+    ];
+  }
+
+  @override
+  Future<List<Property>> getMatchedProperties() async {
+    return [
+      Property(
+        'https://picsum.photos/200/300',
+        ['keywords1', 'keywords2'],
+        '1,500,000.00',
+        '99',
+        '3',
+        '1',
+        '1200 sqft',
+        '',
+        ''
+      ),
+      Property(
+        'https://picsum.photos/200/300',
+        ['one', 'two'],
+        '8,786,123.00',
+        '1',
+        '2',
+        '3',
+        '500 sqft',
+        '',
+        ''
+      ),
+      Property(
+        'https://picsum.photos/200/300',
+        ['qwe', 'asd'],
+        '300,123.12',
+        '9',
+        '8',
+        '7',
+        '654 sqft',
+        '',
+        ''
+      )
+    ];
+  }
+
+  @override
+  Future<List<PhotoTile>> getWhyBrooky() async {
+    return [
+      PhotoTile('', ''),
+      PhotoTile('', ''),
+      PhotoTile('', ''),
+    ];
+  }
+  
+  @override
+  Future<List<Property>> getNewHomes() async {
+    return [
+      Property(
+        'https://picsum.photos/id/237/200/300',
+        ['qqq', 'www'],
+        '9,999,999.99',
+        '81',
+        '41',
+        '0',
+        '100 sqft',
+        '',
+        ''
+      ),
+      Property(
+        'https://picsum.photos/id/132/200/300',
+        ['aaa', 'ssss'],
+        '1,234,567.00',
+        '9',
+        '42',
+        '16',
+        '1,000 sqft',
+        '',
+        ''
+      ),
+      Property(
+        'https://picsum.photos/id/456/200/300',
+        ['ddd', 'dddd'],
+        '540,735.12',
+        '88',
+        '51',
+        '90',
+        '987 sqft',
+        '',
+        ''
+      )
+    ];
   }
   
 }
--- lib/domain/repositories/home_repository.dart
+++ lib/domain/repositories/home_repository.dart
@@ -1,3 +1,14 @@
+import 'package:dazle/domain/entities/photo_tile.dart';
+import 'package:dazle/domain/entities/property.dart';
+
 abstract class HomeRepository {
   Future<void> isNewUser({String email, bool isNewUser});
+
+  Future<List<PhotoTile>> getSpotLight();
+
+  Future<List<Property>> getMatchedProperties();
+
+  Future<List<PhotoTile>> getWhyBrooky();
+
+  Future<List<Property>> getNewHomes();
 }
