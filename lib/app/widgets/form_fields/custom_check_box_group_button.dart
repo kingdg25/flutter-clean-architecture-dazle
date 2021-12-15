19a2cbebdcac230c60f28bb7485f94b76c1479db
Xercis Silao <xercis.silao@gmail.com>
2021-12-15 09:47:23 +0800

    CustomCheckBoxGroupButton

---
+++ lib/app/widgets/form_fields/custom_check_box_group_button.dart
@@ -0,0 +1,44 @@
+import 'package:custom_radio_grouped_button/CustomButtons/ButtonTextStyle.dart';
+import 'package:custom_radio_grouped_button/CustomButtons/CustomCheckBoxGroup.dart';
+import 'package:dazle/app/utils/app.dart';
+import 'package:flutter/material.dart';
+
+class CustomCheckBoxGroupButton extends StatelessWidget {
+  final double checkBoxPadding;
+  final double checkBoxWidth;
+  final List<String> buttonValuesList;
+  final Function checkBoxButtonValues;
+
+  CustomCheckBoxGroupButton({
+    this.checkBoxPadding = 6.0,
+    this.checkBoxWidth = 100.0,
+    @required this.buttonValuesList,
+    @required this.checkBoxButtonValues
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 15),
+      child: CustomCheckBoxGroup(
+        unSelectedBorderColor: App.hintColor,
+        unSelectedColor: Colors.white,
+        selectedBorderColor: App.mainColor,
+        selectedColor: Color.fromRGBO(51, 212, 157, 0.1),
+        buttonTextStyle: ButtonTextStyle(
+          unSelectedColor: App.textColor,
+          selectedColor: App.textColor,
+          textStyle: App.textStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w600
+          ),
+        ),
+        padding: checkBoxPadding,
+        width: checkBoxWidth,
+        buttonLables: buttonValuesList,
+        buttonValuesList: buttonValuesList,
+        checkBoxButtonValues: checkBoxButtonValues
+      ),
+    );
+  }
+}
