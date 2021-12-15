db0af649c6026ba40cb6045975be9b59a1461932
Xercis Silao <xercis.silao@gmail.com>
2021-12-15 17:23:09 +0800

    Amenity model

---
+++ lib/domain/entities/amenity.dart
@@ -0,0 +1,18 @@
+class Amenity {
+  final String text;
+  bool added;
+
+  Amenity({
+    this.text,
+    this.added = false
+  });
+
+  Amenity.fromJson(Map<String, dynamic> json)
+    : text = json['text'],
+      added = json['added'];
+
+  Map<String, dynamic> toJson() => {
+    'text': text,
+    'added': added
+  };
+}
