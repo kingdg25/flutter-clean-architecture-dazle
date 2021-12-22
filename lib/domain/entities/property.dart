5f6271cbba5b5f26dc311d16e3db8c3b469b0ae6
Xercis Silao <xercis.silao@gmail.com>
2021-12-22 09:55:27 +0800

    add amenities,timePeriod,isYourProperty,description

--- lib/domain/entities/property.dart
+++ lib/domain/entities/property.dart
@@ -1,51 +1,68 @@
 class Property {
   final String photoURL;
+  final List amenities;
   final List keywords;
   final String amount;
+  final String timePeriod;
   final String totalBedRoom;
   final String totalBathRoom;
   final String totalParkingSpace;
   final String totalArea;
+  /// furnished or unfurnished
+  final String isYourProperty;
   final String district;
   final String city;
+  final String description;
 
   String get keywordsToString{
     return keywords.join(" • ");
   }
 
 
-  Property(
+  Property({
     this.photoURL,
+    this.amenities,
     this.keywords,
     this.amount,
+    this.timePeriod,
     this.totalBathRoom,
     this.totalBedRoom,
     this.totalParkingSpace,
     this.totalArea,
+    this.isYourProperty,
     this.district,
-    this.city
-  );
+    this.city,
+    this.description
+  });
 
   Property.fromJson(Map<String, dynamic> json)
     : photoURL = json['photo_url'],
+      amenities = json['amenities'],
       keywords = json['keywords'],
       amount = json['amount'],
+      timePeriod = json['time_period'],
       totalBedRoom = json['total_bed_room'],
       totalBathRoom = json['total_bath_room'],
       totalParkingSpace = json['total_parking_space'],
       totalArea = json['total_area'],
+      isYourProperty = json['is_your_property'],
       district = json['district'],
-      city = json['city'];
+      city = json['city'],
+      description = json['description'];
 
   Map<String, dynamic> toJson() => {
     'photo_url': photoURL,
+    'amenities': amenities,
     'keywords': keywords,
     'amount': amount,
+    'time_period': timePeriod,
     'total_bed_room': totalBedRoom,
     'total_bath_room': totalBathRoom,
     'total_parking_space': totalParkingSpace,
     'total_area': totalArea,
+    'is_your_property': isYourProperty,
     'district': district,
-    'city': city
+    'city': city,
+    'description': description
   };
 }
