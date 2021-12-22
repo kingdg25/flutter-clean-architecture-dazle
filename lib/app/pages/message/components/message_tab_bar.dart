6f6ca54b4622a6310d9834111933f17269d27883
Xercis Silao <xercis.silao@gmail.com>
2021-12-22 15:16:29 +0800

    MessageTabBar

---
+++ lib/app/pages/message/components/message_tab_bar.dart
@@ -0,0 +1,61 @@
+import 'package:dazle/app/pages/message_lead/message_lead_view.dart';
+import 'package:dazle/app/pages/message_listing/message_listing_view.dart';
+import 'package:dazle/app/widgets/custom_bottom_tab_bar.dart';
+import 'package:dazle/app/widgets/custom_text.dart';
+import 'package:flutter/material.dart';
+
+class MessageTabBar extends StatefulWidget {
+  const MessageTabBar({ Key key }) : super(key: key);
+
+  @override
+  _MessageTabBarState createState() => _MessageTabBarState();
+}
+
+class _MessageTabBarState extends State<MessageTabBar> with SingleTickerProviderStateMixin {
+  TabController _tabcontroller;
+
+  final List myTabs = <Widget>[
+    Tab(text: 'Listings'),
+    Tab(text: 'Leads'),
+  ];
+
+  @override
+  void initState(){
+    super.initState();
+    _tabcontroller = TabController(vsync: this, length: myTabs.length);
+  }
+
+  @override
+  void dispose(){
+    _tabcontroller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: CustomText(
+          text: 'Inbox',
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+        ),
+        bottom: CustomBottomTabBar(
+          tabController: _tabcontroller,
+          tabs: myTabs,
+        ),
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+      ),
+      backgroundColor: Colors.white,
+      body: TabBarView(
+        controller: _tabcontroller,
+        physics: NeverScrollableScrollPhysics(),
+        children: <Widget>[
+          MessageListingPage(),
+          MessageLeadPage()
+        ],
+      ),
+    );
+  }
+}
--- lib/app/pages/message/message_view.dart
+++ lib/app/pages/message/message_view.dart
@@ -1,5 +1,5 @@
+import 'package:dazle/app/pages/message/components/message_tab_bar.dart';
 import 'package:dazle/app/pages/message/message_controller.dart';
-import 'package:dazle/app/widgets/custom_text.dart';
 import 'package:dazle/data/repositories/data_message_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
@@ -19,21 +19,6 @@ class _MessagePageState extends ViewState<MessagePage, MessageController> {
 
   @override
   Widget get view {
-    return Scaffold(
-      key: globalKey,
-      appBar: AppBar(
-        title: CustomText(
-          text: 'Inbox',
-          fontSize: 20,
-          fontWeight: FontWeight.w600,
-        ),
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.white,
-      ),
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Text('message page'),
-      ),
-    );
+    return MessageTabBar();
   }
 }
