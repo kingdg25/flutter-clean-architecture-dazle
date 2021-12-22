2b9e75d13842c7904396d77486e199ad4fc6ff49
Xercis Silao <xercis.silao@gmail.com>
2021-12-22 11:38:46 +0800

    ListingDetailsContainerBox

---
+++ lib/app/pages/listing_details/components/listing_details_container_box.dart
@@ -0,0 +1,41 @@
+import 'package:dazle/app/utils/app.dart';
+import 'package:dazle/app/widgets/custom_text.dart';
+import 'package:flutter/material.dart';
+
+class ListingDetailsContainerBox extends StatelessWidget {
+  final String asset;
+  final String text;
+
+  ListingDetailsContainerBox({
+    @required this.asset,
+    @required this.text
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: App.hintColor
+        ),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Row(
+        children: [
+          Image.asset(
+            asset
+          ),
+          Flexible(
+            child: CustomText(
+              text: text,
+              fontSize: 12,
+              fontWeight: FontWeight.w600,
+              overflow: TextOverflow.ellipsis,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
