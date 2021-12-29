efff65ce28c124f1a2857c2a018d04e630da75b0
Xercis Silao <xercis.silao@gmail.com>
2021-12-29 10:55:06 +0800

    SettingsListTile

---
+++ lib/app/pages/settings/components/settings_list_tile.dart
@@ -0,0 +1,38 @@
+import 'package:dazle/app/utils/app.dart';
+import 'package:dazle/app/widgets/custom_text.dart';
+import 'package:flutter/material.dart';
+
+class SettingsListTile extends StatelessWidget {
+  final String text;
+  final Function onTap;
+
+
+  SettingsListTile({
+    @required this.text,
+    this.onTap
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: App.hintColor,
+            width: 0.3,
+          )
+        )
+      ),
+      child: ListTile(
+        contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
+        title: CustomText(
+          text: text,
+          fontSize: 13,
+          fontWeight: FontWeight.w700,
+        ),
+        onTap: onTap,
+      ),
+    );
+  }
+}
