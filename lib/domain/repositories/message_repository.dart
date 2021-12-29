f0308f8cbf097e86d662eb5961a09703dd30f8d8
Xercis Silao <xercis.silao@gmail.com>
2021-12-29 16:58:45 +0800

    message listings and leads repository

--- lib/domain/repositories/message_repository.dart
+++ lib/domain/repositories/message_repository.dart
@@ -1,3 +1,7 @@
+import 'package:dazle/domain/entities/message.dart';
+
 abstract class MessageRepository {
-  
+  Future<List<Message>> getMessageListings();
+
+  Future<List<Message>> getMessageLeads();
 }
