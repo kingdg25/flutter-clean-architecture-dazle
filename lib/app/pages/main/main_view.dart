6724f50c64f3cc597b1e64bd804f7df971f78038
luna-john <johnpolluna14@gmail.com>
2022-04-28 14:13:04 +0800

    Updated BottomNavBar

--- lib/app/pages/main/main_view.dart
+++ lib/app/pages/main/main_view.dart
@@ -1,6 +1,6 @@
 import 'package:dazle/app/pages/home/home_view.dart';
 import 'package:dazle/app/pages/listing/listing_view.dart';
-import 'package:dazle/app/pages/main/components/triangle_painter.dart';
+// import 'package:dazle/app/pages/main/components/triangle_painter.dart';
 import 'package:dazle/app/pages/profile/profile_view.dart';
 import 'package:flutter/material.dart';
 
@@ -21,20 +21,21 @@ class _MainPageState extends State<MainPage> {
     HomePage(),
     // ConnectionPage(),
     ListingPage(),
-    // MessagePage(),
+    //MessagePage(),
     ProfilePage(),
   ];
 
-  customBottomNavigationBarItem(
-      {required String asset, required String label}) {
+  customBottomNavigationBarItem({
+    required String asset,
+    required String label,
+    required String icons,
+  }) {
+    print(icons);
     return BottomNavigationBarItem(
-      activeIcon: CustomPaint(
-        painter: TrianglePainter(),
-        child: Container(
-          child: Image.asset(
-            asset,
-            height: 30,
-          ),
+      activeIcon: Container(
+        child: Image.asset(
+          icons,
+          height: 30,
         ),
       ),
       icon: Image.asset(
@@ -51,40 +52,42 @@ class _MainPageState extends State<MainPage> {
       body: _navs[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
+        selectedItemColor: Color.fromRGBO(51, 212, 157, 1),
         currentIndex: _currentIndex,
-        items: [
-          customBottomNavigationBarItem(
-            asset: 'assets/icons/tab_bar/home.png',
-            label: 'home',
-          ),
-
-          // customBottomNavigationBarItem(
-          //   asset: 'assets/icons/tab_bar/connection.png',
-          //   label: 'connection',
-          // ),
-
-          customBottomNavigationBarItem(
-            asset: 'assets/icons/tab_bar/listing.png',
-            label: 'listing',
-          ),
-
-          // customBottomNavigationBarItem(
-          //   asset: 'assets/icons/tab_bar/message.png',
-          //   label: 'message',
-          // ),
-
-          customBottomNavigationBarItem(
-            asset: 'assets/icons/tab_bar/profile.png',
-            label: 'profile',
-          ),
-        ],
         onTap: (index) {
           setState(() {
             _currentIndex = index;
           });
+          print(index);
         },
+        items: [
+          //0
+          customBottomNavigationBarItem(
+            asset: 'assets/icons/tab_bar/clear_home.png',
+            label: 'Home',
+            icons: 'assets/icons/tab_bar/home.png',
+          ),
+          // customBottomNavigationBarItem(
+          //   asset: 'assets/icons/tab_bar/clear_connection.png',
+          //   label: 'Connections',
+          // icons: 'assets/icons/tab_bar/connection.png',
+          // ),
+          customBottomNavigationBarItem(
+            asset: 'assets/icons/tab_bar/clear_listing.png',
+            label: 'New Listing',
+            icons: 'assets/icons/tab_bar/listing.png',
+          ),
+          // customBottomNavigationBarItem(
+          //   asset: 'assets/icons/tab_bar/clear_message.png',
+          //   label: 'Message',
+          //   icon: 'assets/icons/tab_bar/message.png',
+          // ),
+          customBottomNavigationBarItem(
+            asset: 'assets/icons/tab_bar/clear_profile.png',
+            label: 'Profile',
+            icons: 'assets/icons/tab_bar/profile.png',
+          ),
+        ],
       ),
     );
   }
