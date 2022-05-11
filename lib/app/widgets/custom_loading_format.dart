62026339b273fdd6059cb5cb48f716cd21122dff
laevad <djaldave@gmail.com>
2022-05-11 12:32:05 +0800

    fixed my conn and list count in the agent page

--- lib/app/widgets/custom_loading_format.dart
+++ lib/app/widgets/custom_loading_format.dart
@@ -1,40 +1,40 @@
-// import 'package:flutter/material.dart';
-// import 'package:shimmer/shimmer.dart';
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
-// import '../utils/app.dart';
+import '../utils/app.dart';
 
-// class CustomLoadingFormat extends StatelessWidget {
-//   final double? height, width;
-//   final EdgeInsetsGeometry? padding;
-//   final EdgeInsetsGeometry? margin;
-//   final double? radius;
-//   const CustomLoadingFormat({
-//     Key? key,
-//     this.height,
-//     this.width,
-//     this.padding,
-//     this.margin,
-//     this.radius,
-//   }) : super(key: key);
+class CustomLoadingFormat extends StatelessWidget {
+  final double? height, width;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
+  final double? radius;
+  const CustomLoadingFormat({
+    Key? key,
+    this.height,
+    this.width,
+    this.padding,
+    this.margin,
+    this.radius,
+  }) : super(key: key);
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Shimmer.fromColors(
-//       baseColor: App.mainColor,
-//       highlightColor: Colors.white70,
-//       direction: ShimmerDirection.ltr,
-//       child: Container(
-//         margin: margin,
-//         padding: padding,
-//         width: width,
-//         height: height,
-//         decoration: BoxDecoration(
-//           color: App.mainColor.withOpacity(0.3),
-//           borderRadius: BorderRadius.all(
-//             Radius.circular(radius!),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: App.mainColor,
+      highlightColor: Colors.white70,
+      direction: ShimmerDirection.ltr,
+      child: Container(
+        margin: margin,
+        padding: padding,
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: App.mainColor.withOpacity(0.3),
+          borderRadius: BorderRadius.all(
+            Radius.circular(radius!),
+          ),
+        ),
+      ),
+    );
+  }
+}
