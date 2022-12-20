c8495ca7816f0cbe7ff015d684015a33c6dd6758
GioMaghanoy <maghanoygiovane@gmail.com>
2022-12-20 14:31:22 +0800

    adding buid_prod.sh

--- scripts/build_dev.sh
+++ scripts/build_dev.sh
@@ -25,19 +25,15 @@ cd android
 rm Gemfile.lock
 ./gradlew clean assembleRelease
 fvm flutter build appbundle --build-name=$BUILD_NAME --build-number=$BUILD_VERSION --no-tree-shake-icons
-# bundle exec fastlane supply --aab ../build/app/outputs/bundle/release/app-release.aab --track internal
-# python script/helpers/replace.py ios/fastlane/Fastfile "#{BUILD}" "$BUILD_NAME"
-# cd $CI_PROJECT_DIR/ios/
-# rm Gemfile.lock
-# bundle install
-# bundle update fastlane
-# bundle update
-# rm Podfile.lock
-# pod install
-# cp $CI_PROJECT_DIR/ios/GoogleService-staging.plist $CI_PROJECT_DIR/ios/GoogleService-Info.plist
-# cp $CI_PROJECT_DIR/ios/Runner/GoogleService-staging.plist $CI_PROJECT_DIR/ios/Runner/GoogleService-Info.plist
-# cp $CI_PROJECT_DIR/ios/Runner/GoogleService-staging.plist $CI_PROJECT_DIR/ios/GoogleService-Info.plist
-# fastlane staging --verbose
-# cd $CI_PROJECT_DIR
-# python $CI_PROJECT_DIR/script/helpers/upload_release_dev.py $BUILD_NAME && python $CI_PROJECT_DIR/script/helpers/upload_release_dev_ios.py $BUILD_NAME && curl https://us-central1-kahero-8dfde.cloudfunctions.net/setPOSVersion
-     # curl https://asia-east1-pc-api-6370064079833342368-769.cloudfunctions.net/incrementAppVersion?school=sample-school
+bundle exec fastlane supply --aab ../build/app/outputs/bundle/release/app-release.aab --track internal 
+cd ..
+python scripts/helpers/replace.py ios/fastlane/Fastfile "#{BUILD}" "$BUILD_NAME"
+cd $CI_PROJECT_DIR/ios/
+rm Gemfile.lock
+bundle install
+bundle update fastlane
+bundle update
+rm Podfile.lock
+pod install
+fastlane staging --verbose
+curl https://asia-east1-dazle-370306.cloudfunctions.net/incrementAppVersion?company=sample-document
